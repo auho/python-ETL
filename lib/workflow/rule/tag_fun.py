579f0ec3c41a40e29c45f0b739713e87c11a53fd
auho <auho@163.com>
2021-01-06 17:56:32 +0800

    dev

--- lib/workflow/rule/tag_fun.py
+++
@@ -1,8 +0,0 @@
-class TagRule:
-    def __init__(self, fun):
-        self._fun = fun
-
-    def main(self):
-        if not self._fun:
-            if not callable(self._fun):
-                raise Exception("fun is Error!")
