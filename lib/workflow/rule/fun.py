b05df04051843543adad75f9eea46da78ce60824
auho <auho@163.com>
2021-01-06 17:53:41 +0800

    dev

--- lib/workflow/rule/fun.py
+++ lib/workflow/rule/fun.py
@@ -3,6 +3,5 @@ class TagRule:
         self._func = func
 
     def main(self):
-        if not self._func:
-            if not callable(self._func):
-                raise Exception("fun is Error!")
+        if not self._func or not callable(self._func):
+            raise Exception("fun is Error!")
